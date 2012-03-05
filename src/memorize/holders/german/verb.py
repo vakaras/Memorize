e818ba26032daff64de8ceffeaedf6d5a01fdb11
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-03-05 22:40:02 +0200

    Added german verbs support.

---
+++ src/memorize/holders/german/verb.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python
+
+
+""" Information holder for german verbs.
+"""
+
+
+import warnings
+
+import persistent
+
+from memorize.memorizable import Memorizable
+from memorize.log import Logger
+from memorize.holders import word, InformationHolderPlugin
+from memorize.tag_tree import TagList, Tag
+
+
+log = Logger('memorize.holders.german.verb')
+
+
+class VerbMeaning(word.WordMeaning):
+    """ The memorizable meaning of concrete verb.
+    """
+
+
+class ConjugationForm(Memorizable):
+    """ The memorizable verb form.
+
+    .. todo::
+        Implement.
+    """
+
+
+class Verb(word.Word):
+    """ German verb.
+    """
+
+    def __init__(self, infinitive, prefix, *args, **kwargs):
+        super(Verb, self).__init__(None, *args, **kwargs)
+
+        self.infinitive = infinitive
+        self.prefix = prefix
+        # TODO: self.conjugation =
+
+    def __unicode__(self):
+        if self.prefix:
+            return u'{0.prefix} {0.infinitive} ({1})'.format(
+                    self, self.get_id())
+        else:
+            return u'{0.infinitive} ({1})'.format(self, self.get_id())
+
+    def _add_word_meanings(self, meaning, info):
+        """ Adds word meanings.
+        """
+
+        word_meaning = VerbMeaning(
+                self, meaning, info[u'comment'], info[u'examples'])
+
+        self.meanings.setdefault(
+                info[u'translation'], []).append(word_meaning)
+        self.meanings_date[word_meaning.get_date_key()
+                ] = word_meaning
+        self.tag_tree.assign(word_meaning, id_lower_bound=100000)
+        word_meaning.add_tag(Tag(u'word.verb.meaning'))
+        meaning.add_word(self)
+        log.debug(
+                u'Verb {0} has meaning \"{1}\".',
+                unicode(self), word_meaning.meaning.value)
+
+    @property
+    def full_infinitive(self):
+        """ If verb has prefix, then returns prefix + infinitive,
+        otherwise -- just infinitive.
+        """
+        if self.prefix:
+            return self.prefix + self.infinitive
+        else:
+            return self.infinitive
+
+
+class XMLVerbParser(word.XMLWordParser):
+    """ Extracts information about Verbs.
+    """
+
+    def parse(self, node):
+        """ Parses given ``verb`` node.
+        """
+
+        object_id = int(node.get(u'id'))
+        self.manager.mark_object_id(object_id)
+
+        infinitive = unicode(node.get(u'infinitive', u'')).strip()
+        if not infinitive:
+            raise Exception(u'Verb infinitive form must be specified.')
+        prefix = node.get(u'prefix')
+        if prefix:
+            prefix = unicode(prefix).strip()
+
+        tags = TagList(unicode(node.get(u'tags', u'')))
+        tags.append(u'word.verb')
+        transitiveness = node.get(u'transitiveness')
+        if transitiveness == u'transitive':
+            tags.append(u'word.verb.transitive')
+        elif transitiveness == u'intransitive':
+            tags.append(u'word.verb.intransitive')
+        elif transitiveness == u'both':
+            tags.append(u'word.verb.transitive')
+            tags.append(u'word.verb.intransitive')
+        else:
+            raise Exception(u'Verb transitiveness must be specified.')
+        comment = unicode(node.get(u'comment', u''))
+
+        translations = []
+        parts = []
+        examples = []
+        for child in node:
+            if child.tag == u'translation':
+                translations.append({
+                    u'translation': unicode(child.get(u'value')),
+                    u'comment': unicode(child.get(u'comment', u'')),
+                    u'examples': [
+                        int(nr)
+                        for nr in child.get(u'example', u'').split()],
+                    })
+            elif child.tag == u'part':
+                parts.append({
+                    u'child': unicode(child.get(u'value', u'')),
+                    })
+            elif child.tag == u'example':
+                examples.append(word.Example(
+                    int(child.attrib[u'nr']),
+                    unicode(child.attrib[u'org']),
+                    unicode(child.attrib[u'tr'])))
+
+        tree = self.manager.get_tag_tree()
+
+        try:
+            verb = tree.get_object(object_id)
+            warnings.warn(u'Word updating is not implemented. Skipping.')
+        except KeyError:
+            verb = Verb(
+                    infinitive, prefix, comment, parts, translations,
+                    examples)
+            tree.assign(verb, object_id)
+            log.debug(u'TagTree counter: {0}.', tree._counter)
+            for tag in tags:
+                verb.add_tag(tag)
+            self.words_list[object_id] = verb
+            self.write(u'{0}: {1}\n', (u'Adding', 'green'), unicode(verb))
+
+        if prefix:
+            self.words.setdefault(prefix + infinitive, []).append(verb)
+        else:
+            self.words.setdefault(infinitive, []).append(verb)
+
+    def finalize(self):
+        """ Already finalized in Word.
+        """
+
+
+class VerbPlugin(InformationHolderPlugin):
+    """ Registering plugin.
+    """
+
+    tag_name = u'verb'
+    xml_parser = XMLVerbParser
---
+++ src/memorize/manipulators/german/verb.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+
+
+""" Manipulator for german verbs.
+"""
+
+
+import datetime
+
+from memorize.log import Logger
+from memorize.utils import Writer
+from memorize.manipulators import ManipulatorPlugin
+from memorize.tag_tree import TagList, Tag
+from memorize.manipulators import word
+
+
+log = Logger('memorize.manipulators.german.verb')
+
+
+class VerbQuestion(word.WordQuestion):
+    """ Question object for verb.
+    """
+
+    def show(self, file):
+        """ Prints question to file.
+        """
+
+        log.debug(u'Showing WordQuestion.')
+
+        write = Writer(file).write
+
+        write(u'Asking for german verb.\n')
+        if self.word.comment:
+            write(u'Word comment: {0}\n', self.word.comment)
+        if self.word_meaning.examples:
+            write(u'Examples:\n')
+            for nr in self.word_meaning.examples:
+                write(
+                        u'  {0.number}. {0.translation}\n',
+                        self.word.examples[nr])
+        words = len(self.word_meaning.meaning.get_word_list())
+        if words > 1:
+            write(u'{0} words have this meaning. ', words)
+        write(u'Word meaning: {0}\n',
+              (self.word_meaning.meaning.value, 'green'))
+        if self.word_meaning.comment:
+            write(u'Meaning comment: {0}\n', self.word_meaning.comment)
+        write(u'Word meaning tags: {0}.\n',
+                (u' '.join([
+                    unicode(tag)
+                    for tag in self.word_meaning.get_tag_list()
+                    ]),
+                    'green'))
+
+    def check_answer(self, user_answer, write):
+        """ Checks if user answer is correct.
+        """
+        correct = False
+        for verb in self.word_meaning.meaning.get_word_list():
+            if verb.full_infinitive == user_answer:
+                write(u'  Correct answer \"{0}\". All meanings:\n',
+                      (user_answer, 'green'))
+                write(u'    ({0})\n',
+                      u' '.join([
+                          unicode(tag)
+                          for tag in verb.get_tag_list()
+                          ]))
+                for translation in verb.meanings:
+                    write(u'    {0}\n', (translation, 'green'))
+                correct = True
+        if correct:
+            return True
+        else:
+            write(u'  Incorrect answer \"{0}\".\n', (user_answer, 'red'))
+            return False
+
+    def parse_answer(self, answer, file):
+        """ Parses user answer and prints response.
+        """
+
+        log.debug(u'Parsing user answer')
+
+        writer = Writer(file)
+        write = writer.write
+
+        user_answers = [
+                user_answer.strip() for user_answer in answer.split(u'|')]
+        for user_answer in user_answers:
+            self.check_answer(user_answer, write)
+        if self.word.prefix:
+            expected_answer = self.word.prefix + self.word.infinitive
+        else:
+            expected_answer = self.word.infinitive
+
+        write(u'Expected answer was \"{0}\". ', expected_answer)
+        if expected_answer in user_answers:
+            writer.write_string(u'Correct.\n', 'green')
+            self.change_state(self.word, self.word_meaning, 5, write)
+        else:
+            if not answer:
+                writer.write_string(u'No answer.\n', 'red')
+                self.change_state(self.word, self.word_meaning, 0, write)
+            else:
+                writer.write_string(u'Incorrect answer.\n', 'red')
+                self.change_state(self.word, self.word_meaning, 1, write)
+
+        if len(self.word.meanings) > 1:
+            write(u'All meanings of \"{0}\":\n', unicode(self.word))
+            for translation in self.word.meanings:
+                write(u'  {0}\n', (translation, 'green'))
+        write(u'Tags assigned to word: {0}\n',
+              (u' '.join([
+                  unicode(tag) for tag in self.word.get_tag_list()]),
+               'green',))
+
+        if len(self.word_meaning.meaning.words) > 1:
+            write(u'Other words, which have meaning \"{0}\":\n',
+                  self.word_meaning.meaning.value)
+            for word in self.word_meaning.meaning.words.values():
+                if not word is self.word:
+                    write(u'  {0} ({1})\n',
+                          (word, 'green'),
+                          u' '.join([
+                              unicode(tag)
+                              for tag in self.word.get_tag_list()
+                              ]))
+        write(u'\n')
+
+        if self.word.parts:
+            write(u'Parts:\n')
+            for word in self.word.parts.values():
+                write(u'  {0}\n', word)
+                for translation in word.meanings:
+                    write(u'    {0}\n', translation)
+        if self.word_meaning.examples:
+            write(u'Examples:\n')
+            for nr in self.word_meaning.examples:
+                write(
+                        (u'  {0.number}. {0.translation}\n'
+                        u'     {0.original}\n'),
+                        self.word.examples[nr])
+
+
+class VerbManipulatorPlugin(ManipulatorPlugin):
+    """ Manipulator which generates questions for verbs.
+    """
+
+    def __init__(self, plugin_manager):
+        log.debug(u'Called verb manipulator constructor.')
+        super(VerbManipulatorPlugin, self).__init__(plugin_manager)
+
+        self.plugin_manager = plugin_manager
+
+        # Collects words.
+        self.words = self.plugin_manager.tag_tree.get_objects(
+                TagList(
+                    u'word.verb.transitive',
+                    u'word.verb.intransitive',
+                    ))
+
+        # FIXME: Wet code.
+        # Creates questions.
+        today = datetime.date.today()
+        self.questions = []
+        for word in self.words:
+            log.debug(u'Collected word: {0}', word)
+            for date, meaning in word.meanings_date.items():
+                if meaning.get_next_practice().date() <= today:
+                    self.questions.append(VerbQuestion(
+                        self, word, meaning, date))
+                    log.debug(u'Added meaning: {0}.', date)
+                else:
+                    break
+
+    def get_questions(self):
+        """ Returns list of ted questions.
+        """
+
+        log.debug(u'Returning generated questions list.')
+
+        return self.questions
